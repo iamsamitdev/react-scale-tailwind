1c563d1d472626e5a9c6bce36202d40be53c59bf
Samit Koyom <samitkoyom@gmail.com>
2025-01-31 13:56:39 +0700

    initial update detail

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import {Route ,Routes} from 'react-router'
 import Home from "@/pages/Home"
 import About from '@/pages/About'
 import Contact from '@/pages/Contact'
+import ProductDetail from './pages/ProductDetail'
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
+      <Route path="/products/:id" element={<ProductDetail />} />
     </Routes>
   )
 }
